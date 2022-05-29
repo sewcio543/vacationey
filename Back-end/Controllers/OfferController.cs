9d8f338433cd0b4d1bec347bbcdea680e48b7550
sewcio543 <wseweryn@student.agh.edu.pl>
2022-05-29 10:49:01 +0200

    first crud models

---
+++ Back-end/Controllers/OfferController.cs
@@ -0,0 +1,101 @@
+﻿using Backend.Models.DbModels;
+using BookApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class OfferController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: OfferController/Details/5
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            var offer = new Offer();
+            return View(offer);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Offer offer)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DatabaseContext db = new DatabaseContext())
+                {
+                    db.Offer.Add(offer);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(offer);
+        }
+
+        // POST: OfferController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OfferController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OfferController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OfferController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OfferController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
