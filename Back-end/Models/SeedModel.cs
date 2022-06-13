2c4d10ae837e43bd25473811ac4ef75cf17dcf0e
anastazjaspolnik <79855167+anastazjaspolnik@users.noreply.github.com>
2022-06-13 14:07:08 +0200

    Update SeedModel.cs
    Seed - adding data

--- Back-end/Models/SeedModel.cs
+++ Back-end/Models/SeedModel.cs
@@ -24,7 +24,6 @@ namespace Backend.Models
                 var Austria = new Country("Austria");
                 var Belgium = new Country("Belgium");
                 var Turkey = new Country("Turkey");
-                var Brasil = new Country("Brasil");
                 var England = new Country("England");
                 var Ireland = new Country("Ireland");
 
@@ -39,14 +38,19 @@ namespace Backend.Models
                     new City("Valencia", Spain),
                     new City("Madryt", Spain),
                     new City("Bilbao", Spain),
+                    new City("Malaga", Spain),
                     new City("Cracow", Poland),
                     new City("Warsaw", Poland),
                     new City("Gdansk", Poland),
                     new City("Kielce", Poland),
+                    new City("Sopot", Poland),
                     new City("Milan", Italy ),
                     new City("Rome", Italy ),
-                    new City("Mediolan", Italy ),
+                    new City("Palermo", Italy ),
                     new City("Piza", Italy ),
+                    new City("Venice", Italy),
+                    new City("Coimbra", Portugal),
+                    new City("Aveiro", Portugal),
                     new City("Porto", Portugal),
                     new City("Lisboa", Portugal),
                     new City("Braga", Portugal),
@@ -55,8 +59,37 @@ namespace Backend.Models
                     new City("Nicea", France),
                     new City("Lille", France),
                     new City("Nantes", France),
-                    new City("Bordeaux", France),
-                };
+                    new City("Berlin", Germany),
+                    new City("Munich", Germany),
+                    new City("Bonn", Germany),
+                    new City("Frankfurt", Germany),
+                    new City("Hamburg", Germany),
+                    new City("Vienna", Austria),
+                    new City("Graz", Austria),
+                    new City("Salzburg", Austria),
+                    new City("Innsbruck", Austria),
+                    new City("Linz", Austria),
+                    new City("Brussels", Belgium),
+                    new City("Charleroi", Belgium),
+                    new City("Antwerp", Belgium),
+                    new City("Ghent", Belgium),
+                    new City("Liege", Belgium),
+                    new City("Antalya", Turkey),
+                    new City("Istanbul", Turkey),
+                    new City("Ankara", Turkey),
+                    new City("Izmir", Turkey),
+                    new City("Bursa", Turkey),
+                    new City("London", England),
+                    new City("Bristol", England),
+                    new City("Nottingham", England),
+                    new City("Southampton", England),
+                    new City("Leicester", England),
+                    new City("Dublin", Ireland),
+                    new City("Galway", Ireland),
+                    new City("Kinsale", Ireland),
+                    new City("Cork", Ireland),
+                    new City("Clifden", Ireland),
+            };
 
                 var Hotels = new List<Hotel>();
 
@@ -74,7 +107,7 @@ namespace Backend.Models
                     }
                 }
 
-                for (int i = 0; i < 50; i++)
+                for (int i = 0; i < 500; i++)
                 {
                     Random random = new Random();
                     // To
