b29db4be9c558a3c5047b4abc03be3c989a25544
kottoization <96475096+kottoization@users.noreply.github.com>
2022-06-15 19:46:25 +0200

    test15062022

---
+++ Back-end/Controllers/TestError.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Backend.Controllers
+{
+    public class TestError : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
