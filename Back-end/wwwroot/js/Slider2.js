2d7de4280aa0b1b8bf720f09472d318dc7ae111a
kottoization <96475096+kottoization@users.noreply.github.com>
2022-06-16 15:21:23 +0200

    landing page final

---
+++ Back-end/wwwroot/js/Slider2.js
@@ -0,0 +1,28 @@
+﻿let tab = ["~/images/photos/test3.jpg", "~/images/photos/test2.jpg", "~/images/photos/test4.jpg", "~/images/photos/test15.jpg"];
+window.onload = function () {
+    let index1 = Math.floor(Math.random() * 4);
+    let index2 = Math.floor(Math.random() * 4);
+    if (index1 != index2) {
+        let tmp = tab[index1];
+        tab[index1] = tab[index2];
+        tab[index2] = tmp;
+        document.getElementById("slider-p1").src = tab[0];
+        document.getElementById("slider-p2").src = tab[1];
+        document.getElementById("slider-p3").src = tab[2];
+        document.getElementById("slider-p4").src = tab[3];
+
+    }
+}
+document.getElementById('btn1').checked = true;
+var n = 2;
+setInterval(function () {
+    document.getElementById('btn' + n).checked = true;
+    n++;
+    if (n > 4) {
+        n = 1;
+    }
+}, 3000)
+function jeden() { n = 1; }
+function dwa() { n = 2; }
+function trzy() { n = 3; }
+function cztery() { n = 4; }
